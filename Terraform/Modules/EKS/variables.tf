d2919cfda9ffe1a01e1ae64726c01439148c52dd
VarunKRamani <90380089+VarunKRamani@users.noreply.github.com>
2026-04-01 13:19:44 +0530

    Add new variables for EKS configuration
    Added new variables for cluster version, VPC ID, subnet IDs, and node groups to enhance configuration options.

--- Terraform/Modules/EKS/variables.tf
+++ Terraform/Modules/EKS/variables.tf
@@ -1,28 +1,32 @@
-# vpc_cidr - The CIDR block for the VPC, defined as a variable for flexibility.
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-}
-
-# availability_zones - A list of availability zones to distribute the subnets across, defined as a variable for flexibility.
-variable "availability_zones" {
-  description = "Availability zones"
-  type        = list(string)
-}
-
-#private_subnet_cidrs and public_subnet_cidrs - Lists of CIDR blocks for private and public subnets, respectively, defined as variables for flexibility in subnet configuration.
-variable "private_subnet_cidrs" {
-  description = "CIDR blocks for private subnets"
-  type        = list(string)
-}
-
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-}
-
-#cluster_name - The name of the EKS cluster, used in tags for resource identification.
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnet IDs"
+  type        = list(string)
+}
+
+variable "node_groups" {
+  description = "EKS node group configuration"
+  type = map(object({
+    instance_types = list(string)
+    capacity_type  = string
+    scaling_config = object({
+      desired_size = number
+      max_size     = number
+      min_size     = number
+    })
+  }))
 }
